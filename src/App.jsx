32784a14458636da05f713532d1e4e22248ed6b7
SylviaRemington <sylviaremington@gmail.com>
2025-07-23 15:18:21 -0700

    starting with step 1 and creating new state variable named team

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 // src/App.jsx
 
 const App = () => {
+  //Creating a new state variable named team & set initial state to an empty array.
+  const [team, setTeam] = useState();
+  console.log('My team state value is:', team.);
 
   return (
     <h1>Hello world!</h1>
