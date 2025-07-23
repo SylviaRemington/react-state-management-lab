1ccc9700563b045a06ea075ee6bf961aff118ec1
SylviaRemington <sylviaremington@gmail.com>
2025-07-23 15:26:34 -0700

    adding steps 1-3 in lab with adding useState for team & money & zombieFighters

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 // src/App.jsx
 
 const App = () => {
-  //Creating a new state variable named team & set initial state to an empty array.
+  //1. Creating a new state variable named team & setting initial state to an empty array.
   const [team, setTeam] = useState();
-  console.log('My team state value is:', team.);
+  console.log('My team state variable/value is:', team);
+
+  //2. Creating new state variable named money & setting initial state to 100
+  const [money, setMoney] = useState(100);
+
+  //3. Creating new state variable named zombieFighters & setting initial state to the array of objects.
+  const [zombieFighters, setZombieFighters] = useState(fighters);
+
 
   return (
     <h1>Hello world!</h1>
